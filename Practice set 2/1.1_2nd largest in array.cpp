b3c2f4fd7aa781dba83188fbe6bcdeb3df8e239b
Nilay Bhatia <45491806+nilaybhatia@users.noreply.github.com>
2019-04-20 20:04:08 +0530

    Update 1.1_2nd largest in array.cpp
    Fixed wrong array declaration

--- Practice set 2/1.1_2nd largest in array.cpp
+++ Practice set 2/1.1_2nd largest in array.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main () {
 int i, largest, second_largest, n;
-int a[n];
 cout<<"How many elements in array?";
 cin>>n;
+int a[n];
 for(i=0; i<n; i++) cin>>a[i];
 if(a[0]>=a[1]){
   largest=a[0];
@@ -15,6 +15,7 @@ else{
   second_largest=a[0];
 }
 for(i=2; i<n; i++){
+  //cout<<"a[i]= "<<a[i]<<"Largest="<<largest<<" 2nd largest="<<second_largest<<endl;
   if(a[i]>largest){
     second_largest=largest;
     largest=a[i];
